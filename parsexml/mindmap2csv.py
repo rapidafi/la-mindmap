a077ab0303dd06a12a38de2d83899d957cd5faa9
Lauri Jokipii <lauri.jokipii@rapida.fi>
2017-10-24 09:41:57 +0300

    Add ability for command line arguments, especially for subdirectories. Also fix regular expression for extracting user (or student number).

--- parsexml/mindmap2csv.py
+++ parsexml/mindmap2csv.py
@@ -8,13 +8,19 @@ modules in this directory, to ready-named files.
 
 NB! Does not actually provide CSV tool as results are already CSV-like.
 """
-import os, re
+import os, sys, getopt
+import re
 #import ucsv as csv
 
 import mindmapxmltopic, mindmapxmltree, mindmapxmlrelationship
 
+# globally used filehandles
+ftopic = None
+ftree = None
+frelship = None
+
 def handle(folder,item):
-    user = re.sub(r'^(\d+)_.*$',r'\1',item)
+    user = re.sub(r'^([^_]+)_.*$',r'\1',item)
     if not os.path.exists(folder+'\\'+user):
         print("USER DIRECTORY MISSING!",folder,user,item)
     #
@@ -25,23 +31,54 @@ def handle(folder,item):
     ftree.write(treecsv)
     frelship.write(relshipcsv)
 
+def process(folders,extension):
+    global ftopic, ftree, frelship
 
-ftopic = open('mindmaptopic.csv', 'w')
-ftree = open('mindmaptree.csv', 'w')
-frelship = open('mindmaprelationship.csv', 'w')
+    ftopic = open('mindmaptopic.csv', 'w')
+    ftree = open('mindmaptree.csv', 'w')
+    frelship = open('mindmaprelationship.csv', 'w')
 
-ftopic.write(mindmapxmltopic.getheader())
-ftree.write(mindmapxmltree.getheader())
-frelship.write(mindmapxmlrelationship.getheader())
+    ftopic.write(mindmapxmltopic.getheader())
+    ftree.write(mindmapxmltree.getheader())
+    frelship.write(mindmapxmlrelationship.getheader())
 
-folders = ["Viikko1","Viikko2","Viikko3","Viikko4","Viikko5","Viikko6final"]
-extension = ".mmap"
+    for folder in folders:
+        for item in os.listdir(folder):
+            if item.endswith(extension):
+                handle(folder,item)
 
-for folder in folders:
-    for item in os.listdir(folder):
-        if item.endswith(extension):
-            handle(folder,item)
+    ftopic.close()
+    ftree.close()
+    frelship.close()
 
-ftopic.close()
-ftree.close()
-frelship.close()
+def usage():
+    print """
+    Usage:
+
+    mindmap2csv -W|--weeks week1,week2,week3... [-e|--extension .ext] [-d|--debug]
+    """
+
+def main(argv):
+    debug = False
+    weeks = None
+    extension = ".mmap"
+
+    try:
+        opts, args = getopt.getopt(argv,"W:w:e:d",["weeks=","extension=","debug"])
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-W", "-w", "--weeks"): weeks = arg.split(",")
+        elif opt in ("-e", "--extension"): extension = arg
+        elif opt in ("-d", "--debug"): debug = True
+    if not weeks:
+        print "You must give subdirectory names for looping. Exiting"
+        usage()
+        sys.exit(2)
+
+    if debug: print "weeks",weeks
+    process(weeks,extension)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- parsexml/mindmapunzip.py
+++ parsexml/mindmapunzip.py
@@ -6,16 +6,16 @@ mindmapunzip
 Just a helping script to unpack multiple .mmap (zip) files from certain subdirectories.
 """
 
-import os, zipfile, re
+import os, sys, getopt
+import zipfile, re
 
-folders = ["Viikko1","Viikko2","Viikko3","Viikko4","Viikko5","Viikko6final"]
-extension = ".mmap"
+def getheader():
+    print "week;user;item"
 
-print "week;user;item"
 def handle(folder,item):
     os.chdir(folder)
     file_name = os.path.abspath(item) # get full path of files
-    user = re.sub(r'^(\d+)_.*$',r'\1',item)
+    user = re.sub(r'^([^_]+)_.*$',r'\1',item)
     if not os.path.exists(user):
         os.makedirs(user)
     zip_ref = zipfile.ZipFile(file_name) # create zipfile object
@@ -24,8 +24,42 @@ def handle(folder,item):
     print('"%s";"%s";"%s"'%(folder,user,item)) #,file_name
     os.chdir("..")
 
-for folder in folders:
-    for item in os.listdir(folder):
-        if item.endswith(extension):
-            handle(folder,item)
+def process(folders,extension):
+    for folder in folders:
+        for item in os.listdir(folder):
+            if item.endswith(extension):
+                handle(folder,item)
 
+def usage():
+    print """
+    Usage:
+
+    mindmapunzip -W|--weeks week1,week2,week3... [-e|--extension .ext] [-d|--debug]
+    """
+
+def main(argv):
+    debug = False
+    weeks = None
+    extension = ".mmap"
+
+    try:
+        opts, args = getopt.getopt(argv,"W:w:e:d",["weeks=","extension=","debug"])
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-W", "-w", "--weeks"): weeks = arg.split(",")
+        elif opt in ("-e", "--extension"): extension = arg
+        elif opt in ("-d", "--debug"): debug = True
+    if not weeks:
+        print "You must give subdirectory names for looping. Exiting"
+        usage()
+        sys.exit(2)
+
+    if debug: print "weeks",weeks
+    print getheader()
+
+    process(weeks,extension)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
