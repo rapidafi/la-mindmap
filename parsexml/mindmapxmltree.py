dff8644855edeb1037f728d709d20906d51427be
Lauri Jokipii <lauri.jokipii@rapida.fi>
2017-10-24 12:02:22 +0300

    Add floating point topic as parallel ("MSc for BSc" idea). Sanitize.

--- parsexml/mindmapxmltree.py
+++ parsexml/mindmapxmltree.py
@@ -13,66 +13,64 @@ import xml.etree.ElementTree as ET
 # "globals"
 ns = {'ap': 'http://schemas.mindjet.com/MindManager/Application/2003'}
 
-week = None
-user = None
-
-documentcreated = None
-documentlastmodified = None
-documentversion = None
-
 def getheader():
-    return ("week;user;documentCreated;documentLastModified;documentVersion;topic3Oid;topic3PlainText;topic2Oid;topic2PlainText;topic1Oid;topic1PlainText;topic0Oid;topic0PlainText\n").encode('utf-8')
+    ret = "week;user;documentCreated;documentLastModified;documentVersion"
+    ret = ret + ";topic3Oid;topic3PlainText;topic2Oid;topic2PlainText;topic1Oid;topic1PlainText;topic0Oid;topic0PlainText"
+    ret = ret + "\n"
+    return (ret).encode('utf-8')
 
-def gettopic(topic,parents):
+def gettopic(topic):
     topicoid = topic.attrib["OId"]
     topicplaintext = None
 
     for topictext in topic.findall('./ap:Text',ns):
         topicplaintext = topictext.attrib["PlainText"]
     
-    # collect all the parents to same line diff columns, reversed!
-    pstr = ""
+    return (";\"%s\";\"%s\""%(topicoid,topicplaintext))
+
+def getparents(parents):
+    # collect all the parents to same line, reversed!
+    ret = ""
     for p in reversed(parents):
-        pstr = pstr+";\""+p.attrib["OId"]+"\""
-        for topictext in p.findall('./ap:Text',ns):
-            pstr = pstr+";\""+topictext.attrib["PlainText"]+"\""
-
-    return ("%s;%s;%s;%s;%s;\"%s\";\"%s\"%s\n"%
-          (week,user,documentcreated,documentlastmodified,documentversion,
-           topicoid,topicplaintext,pstr)
-          ).encode('utf-8')
+        ret = ret + gettopic(p)
+    return ret
     
-def subtopic(parenttopic,topiclevel,parents):
-    global week,user,documentcreated,documentlastmodified,documentversion
-
+def subtopic(firstcolumns,parenttopic,topiclevel,parents):
     topiclevel = topiclevel + 1
     parents.append(parenttopic)
     ret = ""
     for topic in parenttopic.findall('./ap:SubTopics/ap:Topic',ns):
         if topiclevel == 3:
-            ret = ret + gettopic(topic,parents)
-        ret = ret + subtopic(topic,topiclevel,list(parents))
+            ret = ret + firstcolumns
+            ret = ret + gettopic(topic)
+            ret = ret + getparents(parents)
+            ret = ret + "\n"
+
+        ret = ret + subtopic(firstcolumns,topic,topiclevel,list(parents))
     return ret
 
 # for module usage pass arguments
 def parse(pweek,puser):
-    global week,user,documentcreated,documentlastmodified,documentversion
     week = pweek
     user = puser
 
     tree = ET.parse('.\\'+week+'\\'+user+'\\Document.xml')
     root = tree.getroot()
 
-    ret = ""
     for docgroup in root.findall('.//ap:DocumentGroup',ns):
         documentcreated = docgroup.find('.//ap:DateTimeStamps',ns).attrib["Created"]
         documentlastmodified = docgroup.find('.//ap:DateTimeStamps',ns).attrib["LastModified"]
         documentversion = docgroup.find('.//ap:Version',ns).attrib["Major"]
+    firstcolumns = ("%s;%s;%s;%s;%s"%(week,user,documentcreated,documentlastmodified,documentversion))
+    ret = ""
     for onetopic in root.findall('.//ap:OneTopic',ns):
         for topic in onetopic.findall('./ap:Topic',ns):
-            ret = ret + subtopic(topic,0,[])
-    return ret
-
+            ret = ret + subtopic(firstcolumns,topic,0,[])
+            # floating topics also (MSc for BSc)
+            for floatingtopics in topic.findall('./ap:FloatingTopics',ns):
+                for fttopic in floatingtopics.findall('./ap:Topic',ns):
+                    ret = ret + subtopic(firstcolumns,fttopic,0,[])
+    return ret.encode('utf-8')
 
 def main(argv):
     debug = False
