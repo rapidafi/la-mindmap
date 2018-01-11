f408dfc4e75960a940a1d36df2ff000bad052460
Lauri Jokipii <lauri.jokipii@rapida.fi>
2018-01-11 15:21:26 +0200

    Intermediate commit before cleaning up removes template altogether.

--- parsexml/mindmapxmlgen.py
+++ parsexml/mindmapxmlgen.py
@@ -17,6 +17,7 @@ ns = {
     "ap": "http://schemas.mindjet.com/MindManager/Application/2003",
     "cor": "http://schemas.mindjet.com/MindManager/Core/2003",
     "pri": "http://schemas.mindjet.com/MindManager/Primitive/2003",
+    #"cst": "http://schemas.mindjet.com/MindManager/UpdateCompatibility/2004",
     "xsi": "http://www.w3.org/2001/XMLSchema-instance"
 }
 #xsi:schemaLocation="http://schemas.mindjet.com/MindManager/Application/2003 http://schemas.mindjet.com/MindManager/Application/2003 http://schemas.mindjet.com/MindManager/Core/2003 http://schemas.mindjet.com/MindManager/Core/2003 http://schemas.mindjet.com/MindManager/Delta/2003 http://schemas.mindjet.com/MindManager/Delta/2003 http://schemas.mindjet.com/MindManager/Primitive/2003 http://schemas.mindjet.com/MindManager/Primitive/2003"
@@ -25,6 +26,7 @@ ns = {
 ap = ns['ap'] #comfort
 cor = ns['cor']
 pri = ns['pri']
+#cst = ns['cst']
 xsi = ns['xsi']
 ET.register_namespace('ap',ap)
 ET.register_namespace('cor',cor)
@@ -41,6 +43,26 @@ def randomid(len=16,debug=False):
         if debug: print(i, ret)
     return base64.b64encode(bytearray(ret,'ascii')).decode()
 
+"""Add a Topic under given parent element.
+"""
+def addtopic(parent,plaintext):
+    topic = ET.SubElement(parent, '{%s}Topic'%(ap))
+    topic.set("OId",randomid())
+    #topic.set("Gen","0000000000000000")
+    #topic.set("Dirty","0000000000000001")
+    #NB! Placeholder!
+    subtopics = ET.SubElement(topic, '{%s}SubTopics'%(ap))
+    #topicviewgroup = ET.SubElement(topic, '{%s}TopicViewGroup'%(ap))
+    #topicviewgroup.set("ViewIndex","0")
+    text = ET.SubElement(topic, '{%s}Text'%(ap))
+    text.set("PlainText",plaintext)
+    text.set("ReadOnly","false")
+    #text.set("Dirty","0000000000000000")
+    font = ET.SubElement(text, '{%s}Font'%(ap))
+
+    # return
+    return topic
+
 """Return a pretty-printed XML string for the Element.
 """
 def prettify(elem):
@@ -51,42 +73,30 @@ def prettify(elem):
 ############################################################################
 
 def main(argv):
-    debug = False
+    templatedir = "template0/"
+    documentfile = "Document.xml"
+    zipfile = "mindmapxmlgen.mmap"
     makezip = False
+    debug = False
 
     try:
-        opts, args = getopt.getopt(argv,"zd",["zip","debug"])
+        opts, args = getopt.getopt(argv,"t:f:z:Zd",["templatedir=","documentfile=","zipfile=","makezip","debug"])
     except getopt.GetoptError as err:
         print(err)
         sys.exit(2)
     for opt, arg in opts:
-        if opt in ("-z", "--zip"): makezip = True
+        if opt in ("-t", "--templatedir"): templatedir = arg
+        elif opt in ("-f", "--documentfile"): documentfile = arg
+        elif opt in ("-z", "--zipfile"): zipfile = arg
+        elif opt in ("-Z", "--makezip"): makezip = True
         elif opt in ("-d", "--debug"): debug = True
 
-    #if debug: print("TEST RANDOM/OID GEN:",randomid(debug=debug))
-
-    comment = ET.Comment('Concept Map Analytics: MindMap XML Generated')
     root = ET.Element('{%s}Map'%(ap))
-    root.append(comment)
-
-
-    custom = ET.SubElement(root, '{%s}Custom'%(cor))
-    #custom.text = 'This child contains text & more.'
-    #custom.set("Dirty","0000000000000001")
-    custom.set("OId",randomid())
-    custom.set("Uri","http://schemas.mindjet.com/MindManager/UpdateCompatibility/2004")
-    custom.set("Index","0")
-    #child_with_tail = ET.SubElement(root, '{%s}child_with_tail'%(ap))
-    #child_with_tail.text = 'This child has regular text.'
-    #child_with_tail.tail = 'And "tail" text.'
+    root.set("OId",randomid())
+    ##root.set("xmlns:ap","http://schemas.mindjet.com/MindManager/Application/2003")
 
     onetopic = ET.SubElement(root, '{%s}OneTopic'%(ap))
-    #onetopic.attrib = 'This & that'
 
-    topic0 = ET.SubElement(onetopic, '{%s}Topic'%(ap))
-    topic0.set("OId",randomid())
-
-    subtopics0 = ET.SubElement(topic0, '{%s}SubTopics'%(ap))
     """
     Tutkinto
     - Oppiaine 1
@@ -104,130 +114,51 @@ def main(argv):
     --- Sisältö 7
     --- Sisältö 8
     """
-    if(1==1):
-        topic1 = ET.SubElement(subtopics0, '{%s}Topic'%(ap))
-        topic1.set("OId",randomid())
-        subtopics1 = ET.SubElement(topic1, '{%s}SubTopics'%(ap))
-        if(2==2):
-            topic2 = ET.SubElement(subtopics1, '{%s}Topic'%(ap))
-            topic2.set("OId",randomid())
-            subtopics2 = ET.SubElement(topic2, '{%s}SubTopics'%(ap))
-            if(3==3):
-                topic3 = ET.SubElement(subtopics2, '{%s}Topic'%(ap))
-                topic3.set("OId",randomid())
-                text = ET.SubElement(topic3, '{%s}Text'%(ap))
-                text.set("PlainText","Sisältö 1: Tämän luennon opiskeluun käyttämäni aika")
-                text.set("ReadOnly","false")
-                font = ET.SubElement(text, '{%s}Font'%(ap))
-            if(3==3):
-                topic3 = ET.SubElement(subtopics2, '{%s}Topic'%(ap))
-                topic3.set("OId",randomid())
-                text = ET.SubElement(topic3, '{%s}Text'%(ap))
-                text.set("PlainText","Sisältö 2: Tämän luennon opiskeluun käyttämäni aika")
-                text.set("ReadOnly","false")
-                font = ET.SubElement(text, '{%s}Font'%(ap))
-            text = ET.SubElement(topic2, '{%s}Text'%(ap))
-            text.set("PlainText","Kurssi 1")
-            text.set("ReadOnly","false")
-            font = ET.SubElement(text, '{%s}Font'%(ap))
-        if(2==2):
-            topic2 = ET.SubElement(subtopics1, '{%s}Topic'%(ap))
-            topic2.set("OId",randomid())
-            subtopics2 = ET.SubElement(topic2, '{%s}SubTopics'%(ap))
-            if(3==3):
-                topic3 = ET.SubElement(subtopics2, '{%s}Topic'%(ap))
-                topic3.set("OId",randomid())
-                text = ET.SubElement(topic3, '{%s}Text'%(ap))
-                text.set("PlainText","Sisältö 3: Tämän luennon opiskeluun käyttämäni aika")
-                text.set("ReadOnly","false")
-                font = ET.SubElement(text, '{%s}Font'%(ap))
-            if(3==3):
-                topic3 = ET.SubElement(subtopics2, '{%s}Topic'%(ap))
-                topic3.set("OId",randomid())
-                text = ET.SubElement(topic3, '{%s}Text'%(ap))
-                text.set("PlainText","Sisältö 4: Tämän luennon opiskeluun käyttämäni aika")
-                text.set("ReadOnly","false")
-                font = ET.SubElement(text, '{%s}Font'%(ap))
-            text = ET.SubElement(topic2, '{%s}Text'%(ap))
-            text.set("PlainText","Kurssi 2")
-            text.set("ReadOnly","false")
-            font = ET.SubElement(text, '{%s}Font'%(ap))
-        text = ET.SubElement(topic1, '{%s}Text'%(ap))
-        text.set("PlainText","Oppiaine 1")
-        text.set("ReadOnly","false")
-        font = ET.SubElement(text, '{%s}Font'%(ap))
-    if(1==1):
-        topic1 = ET.SubElement(subtopics0, '{%s}Topic'%(ap))
-        topic1.set("OId",randomid())
-        subtopics1 = ET.SubElement(topic1, '{%s}SubTopics'%(ap))
-        if(2==2):
-            topic2 = ET.SubElement(subtopics1, '{%s}Topic'%(ap))
-            topic2.set("OId",randomid())
-            subtopics2 = ET.SubElement(topic2, '{%s}SubTopics'%(ap))
-            if(3==3):
-                topic3 = ET.SubElement(subtopics2, '{%s}Topic'%(ap))
-                topic3.set("OId",randomid())
-                text = ET.SubElement(topic3, '{%s}Text'%(ap))
-                text.set("PlainText","Sisältö 5: Tämän luennon opiskeluun käyttämäni aika")
-                text.set("ReadOnly","false")
-                font = ET.SubElement(text, '{%s}Font'%(ap))
-            if(3==3):
-                topic3 = ET.SubElement(subtopics2, '{%s}Topic'%(ap))
-                topic3.set("OId",randomid())
-                text = ET.SubElement(topic3, '{%s}Text'%(ap))
-                text.set("PlainText","Sisältö 6: Tämän luennon opiskeluun käyttämäni aika")
-                text.set("ReadOnly","false")
-                font = ET.SubElement(text, '{%s}Font'%(ap))
-            text = ET.SubElement(topic2, '{%s}Text'%(ap))
-            text.set("PlainText","Kurssi 3")
-            text.set("ReadOnly","false")
-            font = ET.SubElement(text, '{%s}Font'%(ap))
-        text = ET.SubElement(topic1, '{%s}Text'%(ap))
-        text.set("PlainText","Oppiaine 2")
-        text.set("ReadOnly","false")
-        font = ET.SubElement(text, '{%s}Font'%(ap))
-        if(2==2):
-            topic2 = ET.SubElement(subtopics1, '{%s}Topic'%(ap))
-            topic2.set("OId",randomid())
-            subtopics2 = ET.SubElement(topic2, '{%s}SubTopics'%(ap))
-            if(3==3):
-                topic3 = ET.SubElement(subtopics2, '{%s}Topic'%(ap))
-                topic3.set("OId",randomid())
-                text = ET.SubElement(topic3, '{%s}Text'%(ap))
-                text.set("PlainText","Sisältö 7: Tämän luennon opiskeluun käyttämäni aika")
-                text.set("ReadOnly","false")
-                font = ET.SubElement(text, '{%s}Font'%(ap))
-            if(3==3):
-                topic3 = ET.SubElement(subtopics2, '{%s}Topic'%(ap))
-                topic3.set("OId",randomid())
-                text = ET.SubElement(topic3, '{%s}Text'%(ap))
-                text.set("PlainText","Sisältö 8: Tämän luennon opiskeluun käyttämäni aika")
-                text.set("ReadOnly","false")
-                font = ET.SubElement(text, '{%s}Font'%(ap))
-            text = ET.SubElement(topic2, '{%s}Text'%(ap))
-            text.set("PlainText","Kurssi 4")
-            text.set("ReadOnly","false")
-            font = ET.SubElement(text, '{%s}Font'%(ap))
 
-    text = ET.SubElement(topic0, '{%s}Text'%(ap))
-    text.set("PlainText","Koneenrakennus ja mekaniikka")
-    text.set("ReadOnly","false")
-    font = ET.SubElement(text, '{%s}Font'%(ap))
+    topic0 = addtopic(onetopic, "Koneenrakennus ja mekaniikka")
+    for subtopics0 in topic0.findall('.//ap:SubTopics',ns):
+        topic1 = addtopic(subtopics0, "Oppiaine 1")
+        for subtopics1 in topic1.findall('.//ap:SubTopics',ns):
+            topic2 = addtopic(subtopics1,"Kurssi 1")
+            for subtopics2 in topic2.findall('.//ap:SubTopics',ns):
+                addtopic(subtopics2,"Sisältö 1")
+                addtopic(subtopics2,"Sisältö 2")
+            topic2 = addtopic(subtopics1, "Kurssi 2")
+            for subtopics2 in topic2.findall('.//ap:SubTopics',ns):
+                addtopic(subtopics2, "Sisältö 3")
+                addtopic(subtopics2, "Sisältö 4")
+        topic1 = addtopic(subtopics0, "Oppiaine 2")
+        for subtopics1 in topic1.findall('.//ap:SubTopics',ns):
+            topic2 = addtopic(subtopics1,"Kurssi 3")
+            for subtopics2 in topic2.findall('.//ap:SubTopics',ns):
+                addtopic(subtopics2,"Sisältö 5")
+                addtopic(subtopics2,"Sisältö 6")
+            topic2 = addtopic(subtopics1, "Kurssi 4")
+            for subtopics2 in topic2.findall('.//ap:SubTopics',ns):
+                addtopic(subtopics2, "Sisältö 7")
+                addtopic(subtopics2, "Sisältö 8")
 
-    #print(ET.tostring(root).decode('utf-8'))
     if debug: print(prettify(root))
 
     if makezip:
-        file="Document.xml"
-        zipfile="mindmapxmlgen.mmap"
-        print("writing to file:",file)
-        f = open(file, 'w', encoding='utf-8')
+        xsddir='xsd/'
+        backgroundfile='bin/background.png'
+        print("writing to file:",documentfile)
+        f = open(documentfile, 'w', encoding='utf-8')
+        #f.write(ET.tostring(root).decode('utf-8'))
         f.write(prettify(root))
         f.close()
 
         print("zipping to file:",zipfile)
         with ZipFile(zipfile,'w',compression=ZIP_DEFLATED) as z:
-            z.write(file)
+            if debug: print("zipping:",documentfile)
+            z.write(documentfile)
+            #for fn in os.listdir(templatedir+xsddir):
+            #    if os.path.isfile(templatedir+xsddir+fn):
+            #        if debug: print("zipping:",templatedir+xsddir+fn)
+            #        z.write(filename=templatedir+xsddir+fn,arcname=xsddir+fn)
+            #if debug: print("zipping:",templatedir+backgroundfile)
+            #z.write(filename=templatedir+backgroundfile,arcname=backgroundfile)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
